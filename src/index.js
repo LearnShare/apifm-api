bcf9d290bc0b020220676093d1e629e299c308f2
LearnShare <learnshare@126.com>
2022-01-25 14:27:34 +0800

    feat: backend APIs.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 /* eslint-disable */
-// 小程序开发api接口工具包，https://github.com/gooking/wxapi
-var API_BASE_URL = 'https://user.api.it120.cc'
-var subDomain = '-'
-var merchantId = '0'
+var API_BASE_URL = 'https://user.api.it120.cc';
+var subDomain = '-';
+var merchantId = '0';
+var headers = {};
 
 let request = (url, needSubDomain, method, data) => {
-  const _url = API_BASE_URL + (needSubDomain ? '/' + subDomain : '') + url
-  const header = {
-    'Content-Type': 'application/x-www-form-urlencoded'
-  }
+  const _url = API_BASE_URL + (needSubDomain ? '/' + subDomain : '') + url;
+  const header = headers;
+  header['Content-Type'] = 'application/x-www-form-urlencoded';
+
   return new Promise((resolve, reject) => {
     wx.request({
       url: _url,
@@ -23,59 +23,35 @@ let request = (url, needSubDomain, method, data) => {
       },
       complete(aaa) {
         // 加载完成
-      }
-    })
-  })
+      },
+    });
+  });
 }
 
-/**
- * 小程序的promise没有finally方法，自己扩展下
- */
-// Promise.prototype.finally = function (callback) {
-//   var Promise = this.constructor;
-//   return this.then(
-//     function (value) {
-//       Promise.resolve(callback()).then(
-//         function () {
-//           return value;
-//         }
-//       );
-//     },
-//     function (reason) {
-//       Promise.resolve(callback()).then(
-//         function () {
-//           throw reason;
-//         }
-//       );
-//     }
-//   );
-// }
-
 module.exports = {
-  init2: (a, b) => {
-    API_BASE_URL = a
-    subDomain = b
-  },
-  init: (b) => {
-    subDomain = b
-  },
-  setMerchantId: (mchid) => {
-    merchantId = mchid
-  },
-  init3: ({
-    apiBaseUrl = API_BASE_URL,
-    subDomain: subD,
-    request: req,
-  }) => {
-    // 某些需求需要定制化 request，需要保证传入自定义 reuqest 与默认 request 参数一致
-    if (req) {
-      request = req
-    }
-    API_BASE_URL = apiBaseUrl
-    subDomain = subD
+  init: (token) => {
+    headers = {
+      'X-Token': token,
+    };
   },
   request,
+
+  login: (data) => {
+    return request('/login/userName/v2', false, 'post', data);
+  },
+  logout: (token) => {
+    return request(`/login/exit?${token}`, false, 'get');
+  },
+  getAccountInfo: (data) => {
+    return request('/user/info', false, 'get');
+  },
   orderList: (data) => {
-    return request('/user/apiExtOrder/list', false, 'post', data)
+    return request('/user/apiExtOrder/list', false, 'post', data);
+  },
+  orderDetail: (id) => {
+    return request(`/user/apiExtOrder/detail?id=${id}`, false, 'get');
+  },
+  startDelivery: (data) => {
+    return request('/user/apiExtOrder/fahuo', false, 'post', data);
   },
 }
